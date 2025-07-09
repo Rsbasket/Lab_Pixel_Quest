cb5ae93c317a77749cdbd01e719f2071c99ac738
Rsbasket <rsbasket3@gmail.com>
2025-07-09 12:11:16 -0400

    Code
    7/9/25

---
+++ Assets/Geo_Quest/Scripts/GeoQScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class GeoQScript : MonoBehaviour{
+    int var = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(var);
+        var++;
+    }
+}
