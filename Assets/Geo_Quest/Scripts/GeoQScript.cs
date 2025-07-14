adc604d7c84428464e9d4ab42c862305cf1e1c8d
Rsbasket <rsbasket3@gmail.com>
2025-07-14 15:50:05 -0400

    jumping mechanic
    jumping, death and new scene

--- Assets/Geo_Quest/Scripts/GeoQScript.cs
+++ Assets/Geo_Quest/Scripts/GeoQScript.cs
@@ -1,56 +1,65 @@
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GeoQScript : MonoBehaviour 
+public class GeoQScript : MonoBehaviour
 {
     int var = 3;
     public int speed = 9;
-
+    public float jumpForce = 12f; // Added jump force
+    public string nextLevel = "Scene_2";
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private bool isGrounded = false; // Track if player is on the ground
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-
-
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        // Jumping with 'W'
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Simple ground check: tag your ground objects as "Ground"
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Hit");
         switch (collision.tag)
-      
         {
-           
             case "Win":
-                {
-                    break;
-                }
-            case "Lose":   
-                {
-                    break;
-                }
+                break;
+            case "Lose":
+                break;
             case "Death":
-                {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
-                }
-          
-
+                string thisLevel = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(thisLevel);
+                break;
+            case "Finish":
+                SceneManager.LoadScene(nextLevel);
+                break;
         }
     }
-
-
 }
