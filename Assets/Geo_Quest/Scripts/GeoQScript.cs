e5d41b4e8d9870cfb58ad47f3aa9fca4da0ff1af
Rsbasket <rsbasket3@gmail.com>
2025-07-15 12:18:07 -0400

    completed levels

--- Assets/Geo_Quest/Scripts/GeoQScript.cs
+++ Assets/Geo_Quest/Scripts/GeoQScript.cs
@@ -5,10 +5,8 @@ public class GeoQScript : MonoBehaviour
 {
     int var = 3;
     public int speed = 9;
-    public float jumpForce = 12f; // Added jump force
     public string nextLevel = "Scene_2";
     private Rigidbody2D rb;
-    private bool isGrounded = false; // Track if player is on the ground
 
     void Start()
     {
@@ -19,29 +17,6 @@ public class GeoQScript : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
-        // Jumping with 'W'
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            isGrounded = false;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Simple ground check: tag your ground objects as "Ground"
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
